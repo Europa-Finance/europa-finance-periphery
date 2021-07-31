15d6556d35d16257888d71ef12851814e40569d0
thekid-lestrange <thekid.lestrange@gmail.com>
2021-07-31 08:27:01 +0700

    europa router update

--- contracts/examples/ExampleFlashSwap.sol
+++ contracts/examples/ExampleFlashSwap.sol
@@ -1,6 +1,6 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IPancakeCallee.sol';
+import '@europa-finance/europa-finance-core/contracts/interfaces/IPancakeCallee.sol';
 
 import '../libraries/PancakeLibrary.sol';
 import '../interfaces/V1/IUniswapV1Factory.sol';
